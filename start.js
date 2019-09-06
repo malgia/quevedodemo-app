6789da2ea875e368efe3d8c00de4ccc1282584db
Oscar Malgiaritta <malgia@hotmail.com>
2019-09-05 23:33:54 -0500

    fixed port

--- start.js
+++ start.js
@@ -16,6 +16,6 @@ require('./models/Motorcycle');
 const app = require('./app');
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
